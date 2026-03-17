552285e7c95ea28c6ccae569b4e22009ee0eaf16
Matheus Silva <matheus1882014@outlook.com>
2026-03-16 21:27:55 -0300

    removendo componentes dos textos para testar se fica melhor

--- App.tsx
+++ App.tsx
@@ -39,10 +39,8 @@ const App: React.FC = () => {
               <br />
             </h1>
             <p className="text-slate-400 text-lg md:text-xl leading-relaxed mb-8 max-w-2xl">
-              Olá,eu sou Matheus
-              <span className="text-cyan-400 font-semibold">
-                estudante de Dados e Engenharia de Machine Learning
-              </span>
+              Olá,eu sou Matheus, estudante de Dados e Engenharia de Machine Learning
+              
                  Estudo Análise e Desenvolvimento de Sistemas e tenho uma abordagem centrada em{" "}
               <span className="text-cyan-400 font-semibold">Python</span> para construir sistemas de machine
               learning e aplicações orientadas por dados.
